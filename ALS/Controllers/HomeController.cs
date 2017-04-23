836724ed0dce601b86e8d0f5d3afee858c6f6f37
yavorvasilev <yavor.vasilev666@abv.bg>
2017-04-23 21:45:48 +0300

    Create first look on Homepage.

--- ALS/Controllers/HomeController.cs
+++ ALS/Controllers/HomeController.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace ALS.Controllers
+﻿namespace ALS.Controllers
 {
+    using Data;
+    using Models.Ad;
+    using System.Linq;
+    using System.Web.Mvc;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             //return RedirectToAction("List", "Ad");
-            return View();
+            var database = new AdsDbContext();
+
+            var ads = database.Ads
+                .OrderByDescending(a => a.Id)
+                .Take(3)
+                .Select(a => new AdListingModel
+                {
+                    Id = a.Id,
+                    Title = a.Title,
+                    Category = a.Category,
+                    Price = a.Price,
+                    MainPicture = a.Pictures.FirstOrDefault()
+                })
+                .ToList();
+
+            return View(ads);
         }
 
     }
